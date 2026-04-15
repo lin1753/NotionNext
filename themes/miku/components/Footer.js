58acd508701f7cc851d8517fdbe35de3f81fc178
mikuhhn <2510922813@qq.com>
2026-04-15 16:10:48 +0800

    feat(miku): clean up footer, show only 2025-2026 and pageviews

--- themes/miku/components/Footer.js
+++ themes/miku/components/Footer.js
@@ -11,20 +11,13 @@ const Footer = ({ title }) => {
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-miku-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'>
+    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-transparent justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'>
       {/* <DarkModeButton/> */}
       <i className='fas fa-copyright' /> {`${copyrightDate}`}
       <span>
-        <i className='mx-1 animate-pulse fas fa-heart' />
-        <a
-          href={siteConfig('LINK')}
-          className='underline font-bold  dark:text-gray-300 '>
-          {siteConfig('AUTHOR')}
-        </a>
-        .<br />
         <BeiAnSite />
         <BeiAnGongAn />
-        <span className='hidden busuanzi_container_site_pv'>
+        <span className='hidden busuanzi_container_site_pv' style={{ marginLeft: '10px' }}>
           <i className='fas fa-eye' />
           <span className='px-1 busuanzi_value_site_pv'> </span>
         </span>
@@ -32,10 +25,6 @@ const Footer = ({ title }) => {
           <i className='fas fa-users' />
           <span className='px-1 busuanzi_value_site_uv'> </span>
         </span>
-        <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>
-          {title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}
-        </h1>
-        <PoweredBy className='justify-center' />
       </span>
       <br />
     </footer>
